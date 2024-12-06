deb23f21bdce2044eb04af427320b4cd791f5885
Yanis <yanis@srairi.fr>
2024-12-06 11:32:41 +0100

    fix: escape not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
     return (
       <div style={{ textAlign: 'center', marginTop: '50px' }}>
         <h1>404 - Page non trouvée</h1>
-        <p>La base que vous recherchez n'existe pas.</p>
+        <p>La base que vous recherchez n&#39;existe pas.</p>
       </div>
     );
   }
